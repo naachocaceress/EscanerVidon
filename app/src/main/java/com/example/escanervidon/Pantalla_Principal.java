239658449c8c4969858d7506efd86d618ebb1066
naachocaceress <naachocaceress@gmail.com>
2023-09-26 02:20:16 -0300

    Version 1.4.2 - Migracion a nuevo dominio

--- app/src/main/java/com/example/escanervidon/Pantalla_Principal.java
+++ app/src/main/java/com/example/escanervidon/Pantalla_Principal.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Pantalla_Principal extends AppCompatActivity {
-    String version_actual = "1.4.1";
+    String version_actual = "1.4.2";
     String version_firebase;
     String url_firebase;
 
